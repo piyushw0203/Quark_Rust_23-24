5a308b4057fd9d4e6a7e91590937143ad7daf9b8
Shivansh <shivansh.saraswat.2304@gmail.com>
2023-09-17 12:22:54 +0530

    Task 3 complete

---
+++ Task 3 Shivansh/target/debug/deps/menu.d
@@ -0,0 +1,5 @@
+C:\Everything\Projects\VS Code\Rust\menu\target\debug\deps\menu.exe: src\main.rs
+
+C:\Everything\Projects\VS Code\Rust\menu\target\debug\deps\menu.d: src\main.rs
+
+src\main.rs:
---
+++ Task 3 Shivansh/target/debug/menu.d
@@ -0,0 +1,1 @@
+C:\Everything\Projects\VS\ Code\Rust\menu\target\debug\menu.exe: C:\Everything\Projects\VS\ Code\Rust\menu\src\main.rs
